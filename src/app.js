f381e15f38a6c9d4b6e4abe9cf1c50ee0613c4c1
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-03-02 13:56:11 +0100

    working on initial code structure

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+import productsRouter from "./routes/products.js";
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Webbshop API", stack: "MEN (MongoDB, Express, Node.js)" });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.use("/products", productsRouter);
+//TODO: Add more routes as needed
+
+export { app };
---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //TODO: Add salt and hash password
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
