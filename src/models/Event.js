e411979d50a6a23a7683daa1d23a11b0dda389cc
emmapalm88 <emmapalm88@gmail.com>
2026-04-08 11:06:10 +0200

    ändringar i event model

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
-  title: String,
+  title: { type: String, required: true },
   description: String,
-  totalSpots: Number,
+  date: { type: Date, required: true },
+  location: String,
+  totalSpots: { type: Number, required: true },
   bookedSpots: {
     type: Number,
     default: 0
-  }
+  },
+  category: String
 });
 
 export default mongoose.model("Event", eventSchema);
